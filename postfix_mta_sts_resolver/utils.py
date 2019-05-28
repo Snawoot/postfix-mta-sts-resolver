70312b4780c8c5c4c14a1fe52c358a67cce761e4
Vladislav Yarmak <vladislav@vm-0.com>
2019-05-28 17:57:58 +0300

    throw out unused routines

--- postfix_mta_sts_resolver/utils.py
+++ postfix_mta_sts_resolver/utils.py
@@ -41,22 +41,6 @@ def setup_logger(name, verbosity, logfile=None):
     return logger
 
 
-def check_port(value):
-    ivalue = int(value)
-    if not 0 < ivalue < 65536:
-        raise argparse.ArgumentTypeError(
-            "%s is not a valid port number" % value)
-    return ivalue
-
-
-def check_positive_float(value):
-    fvalue = float(value)
-    if fvalue <= 0:
-        raise argparse.ArgumentTypeError(
-            "%s is not a valid value" % value)
-    return fvalue
-
-
 def enable_uvloop():
     try:
         import uvloop
