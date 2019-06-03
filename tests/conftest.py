69dab9929fce9208490e93c171f94127c6f9ac43
Vladislav Yarmak <vladislav@vm-0.com>
2019-06-03 15:12:48 +0300

    tests: cover uvloop

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
-import pytest
 import asyncio
+import os
+
+import pytest
+
+from postfix_mta_sts_resolver.utils import enable_uvloop
 
 @pytest.fixture(scope="session")
 def event_loop():
+    uvloop_test = os.environ['TOXENV'].endswith('-uvloop')
+    uvloop_enabled = enable_uvloop()
+    assert uvloop_test == uvloop_enabled
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
