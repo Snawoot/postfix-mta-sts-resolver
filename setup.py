0953fc05644034564245a6a360396d3946d40bd5
Vladislav Yarmak <vladislav@vm-0.com>
2020-01-12 07:30:12 +0200

    finished new sdnotify

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(name='postfix_mta_sts_resolver',
           'aiodns>=1.1.1',
           'aiohttp>=3.4.4',
           'PyYAML>=3.12',
-          'sdnotify>=0.3.2',
       ],
       extras_require={
           'sqlite': 'aiosqlite>=0.10.0',
